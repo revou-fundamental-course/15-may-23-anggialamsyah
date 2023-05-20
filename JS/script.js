2b65fe87dbb0e57d959c3149e24bcd3b48119939
anggialamsyah <anggialamsyah2@gmail.com>
2023-05-21 00:36:58 +0700

    add somejavascipt

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+function hitungluas() {
+    
+    var sisiluas = document.getElementById("sisi-luas").value;
+   
+    var style = document.getElementsByName("result-luas").style 
+    console.log(sisiluas);
+    document.getElementById("output-luas").innerHTML = '<span>'+sisiluas+'</span>';
+}
+    
+    
+
+
